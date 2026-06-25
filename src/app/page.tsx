adb04d9008fddeffc2d3e6aba2258b6ae3572f02
Marc Beltran <marcbeltran4@gmail.com>
2026-06-25 17:03:34 +0200

    remove horitzontal padding in pc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ChatClient from "@/components/ChatClient";
 export default function Home() {
   return (
     <main className="flex h-dvh items-center justify-center bg-[#dfe5e7]">
-      <div className="flex h-full w-full max-w-[1600px] flex-col shadow-xl lg:h-[96vh] lg:rounded-sm">
+      <div className="flex h-full w-full max-w-[1600px] flex-col shadow-xl">
         <ChatClient />
       </div>
     </main>
